4ace38f179dfdcaf9d534355c55a51a76e14dae7
Victor Leach <victor96@mac.com>
2017-01-06 11:12:10 +0000

    Moved server to web pack building.

--- tasks/server/build.js
+++ tasks/server/build.js
@@ -1,48 +1,67 @@
 //Modules
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
+const gulp = require('gulp')
+const path = require('path')
+const fs = require('fs')
+const stream = require('webpack-stream')
+const webpack = require('webpack')
+const named = require('vinyl-named')
+const through = require('through2')
+const sourcemaps = require('gulp-sourcemaps')
+const obfuscator = require('webpack-obfuscator')
 
-/*! Tasks 
+//Config
+let config = require('../../config.js')
+
+//Compile list of node modules for webpack
+let nodeModules = {}
+fs.readdirSync('node_modules').filter(function(x) { return ['.bin'].indexOf(x) === -1 }).forEach(function(mod) { nodeModules[mod] = 'commonjs ' + mod })
+nodeModules['config'] = 'commonjs ../config.js'
+
+/*! Tasks
 - server.build
-
-- server.build.source
-- server.build.typescript
 */
 
 //! Build
-gulp.task('server.build', gulp.parallel('server.build.source', 'server.build.typescript'));
 
-//Copy over source files
-gulp.task('server.build.source', function(){
-	return gulp.src([
-		'server/**/*',
-		'!server/**/*.md',
-		'!server/**/*.ts',
-		'!server/typings.json',
-		'!server/typings',
-		'!server/typings/**/*'
-	])
-	.pipe(gulp.dest('builds/server'));
-});
-
-//Create typescript project
-let tsProject = ts.createProject({
-	typescript: require('typescript'),
-	target: 'es5',
-	module: 'commonjs',
-	moduleResolution: 'node',
-	sourceMap: true,
-	emitDecoratorMetadata: true,
-	experimentalDecorators: true,
-	removeComments: true,
-	noImplicitAny: true,
-	suppressImplicitAnyIndexErrors: true,
-	sortOutput: true
-});
-
-//Compile typescript into javascript
-gulp.task('server.build.typescript', function() {
-	return gulp.src('**/*.ts', { cwd: 'server' })
-	.pipe(ts(tsProject))
-	.pipe(gulp.dest('builds/server'))
-});
+gulp.task('server.build', function(){
+	
+	let plugins = [
+	//	new webpack.optimize.UglifyJsPlugin(),
+	//	new obfuscator()
+	];
+	
+	
+	
+	return gulp.src('./server/index.js')
+		.pipe(named())
+		.pipe(stream({
+			target: 'node',
+			devtool: 'inline-source-map',
+			externals: nodeModules,
+			plugins: plugins,
+			output: {
+				filename: 'index.js'
+			},
+			resolve: {
+				modules: [ 'server', 'node_modules' ]
+			},
+			module: {
+				rules: [{
+					test: /\.js$/,
+					exclude: /(node_modules|bower_components)/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['es2015']
+					}
+				}]
+			},
+		}, webpack))
+		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(through.obj(function (file, enc, cb) {
+			var isSourceMap = /\.map$/.test(file.path);
+			if (!isSourceMap) this.push(file);
+			cb();
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('builds/server'))
+})
