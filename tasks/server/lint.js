9ab490adc44cac7e3191c462253dd5fb0cf842d3
Victor Leach <victor96@mac.com>
2017-01-06 11:11:58 +0000

    Added ES6 linking to server.

--- tasks/server/lint.js
+++ tasks/server/lint.js
@@ -1,47 +1,23 @@
 //Modules
 const gulp = require('gulp');
-const tslint = require('gulp-tslint');
 const jshint = require('gulp-jshint');
 
 /*! Tasks 
 - server.lint
 
-- server.lint.javascript
-- server.lint.typescript
+- server.lint.css
+- server.lint.js
 */
 
 //! Lint
-gulp.task('server.lint', gulp.parallel('server.lint.javascript', 'server.lint.typescript'));
+gulp.task('server.lint', gulp.parallel('server.lint.js'));
 
-//Check javascript for lint
-gulp.task('server.lint.javascript', function(){
-	return gulp.src([
-		'server/**/*.js',
-		'!server/**/*.min.js',
-		'!server/typings/**/*'
-	])
-	.pipe(jshint({
-		esversion: 6
-	}))
-    .pipe(jshint.reporter('default'));
-});
-
-//Check typescript lint
-gulp.task('server.lint.typescript', function(){
-	return gulp.src([
-		'server/**/*.ts',
-		'!server/typings/**/*'
-	])
-	.pipe(tslint({
-        configuration: {
-	        rules: {
-				'no-duplicate-key': true,
-				'no-duplicate-letiable': true,
-				'semicolon': true
-	        }
-        }
-    }))
-    .pipe(tslint.report('verbose', {
-	    emitError: false
-    }));
+//Validate javascript syntax
+gulp.task('server.lint.js', function(){
+	return gulp.src('server/**/*.js')
+		.pipe(jshint({
+			esversion: 6,
+			asi: true
+		}))
+	    .pipe(jshint.reporter());
 });
