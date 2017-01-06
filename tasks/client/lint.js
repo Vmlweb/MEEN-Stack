101b4493594c0db2a569a1d83d924d328774f551
Victor Leach <victor96@mac.com>
2017-01-06 13:08:13 +0000

    Added JSON linting.

--- tasks/client/lint.js
+++ tasks/client/lint.js
@@ -1,7 +1,7 @@
 //Modules
-const gulp = require('gulp');
-const jshint = require('gulp-jshint');
-const csslint = require('gulp-csslint');
+const gulp = require('gulp')
+const jshint = require('gulp-jshint')
+const csslint = require('gulp-csslint')
 
 /*! Tasks 
 - client.lint
@@ -11,21 +11,24 @@ const csslint = require('gulp-csslint');
 */
 
 //! Lint
-gulp.task('client.lint', gulp.parallel('client.lint.css', 'client.lint.js'));
+gulp.task('client.lint', gulp.parallel('client.lint.css', 'client.lint.js'))
 
 //Validate css syntax
 gulp.task('client.lint.css', function(){
 	return gulp.src('client/**/*.css')
 	    .pipe(csslint())
-	    .pipe(csslint.formatter());
-});
+	    .pipe(csslint.formatter())
+})
 
 //Validate javascript syntax
 gulp.task('client.lint.js', function(){
-	return gulp.src('client/**/*.js')
-		.pipe(jshint({
-			esversion: 6,
-			asi: true
-		}))
-	    .pipe(jshint.reporter());
-});
+	return gulp.src([
+		'client/**/*.js',
+		'client/**/*.json'
+	])
+	.pipe(jshint({
+		esversion: 6,
+		asi: true
+	}))
+    .pipe(jshint.reporter())
+})
--- tasks/server/lint.js
+++ tasks/server/lint.js
@@ -1,23 +1,20 @@
 //Modules
-const gulp = require('gulp');
-const jshint = require('gulp-jshint');
+const gulp = require('gulp')
+const jshint = require('gulp-jshint')
 
 /*! Tasks 
 - server.lint
-
-- server.lint.css
-- server.lint.js
 */
 
 //! Lint
-gulp.task('server.lint', gulp.parallel('server.lint.js'));
-
-//Validate javascript syntax
-gulp.task('server.lint.js', function(){
-	return gulp.src('server/**/*.js')
-		.pipe(jshint({
-			esversion: 6,
-			asi: true
-		}))
-	    .pipe(jshint.reporter());
-});
+gulp.task('server.lint', function(){
+	return gulp.src([
+		'server/**/*.js',
+		'server/**/*.json'
+	])
+	.pipe(jshint({
+		esversion: 6,
+		asi: true
+	}))
+    .pipe(jshint.reporter())
+})
